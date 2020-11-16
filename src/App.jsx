5a6c3bb6605be216ae2d0eca3efd11a32074857d
floriansr <sueurfloriansf@gmail.com>
2020-11-16 17:00:23 +0100

    [update] App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import Cookies from 'js-cookie';
+
+// React-router-dom library to handle navigation
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Hooks
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// Redux action
+import { setUserData, removeUserData } from './redux';
+
+// Component
 import Navbar from './components/Navbar';
+import Callback from './components/Callback';
+
+// Tool
 import Authroute from './tools/Authroute';
 
+// Class Managers
+import TokenManager from './services/TokenManager';
+import SpotifyManager from './services/SpotifyAPIManager';
+
+// Page components
 import Register from './pages/Register';
 import LogIn from './pages/Login';
 import Home from './pages/Home';
@@ -13,12 +32,61 @@ import NotFound from './pages/NotFound';
 import Playlist from './pages/Playlist';
 import NewPlaylist from './pages/NewPlaylist';
 
+/*
+  The 'heart' of the program with the following features:
+  1) Store the state and user details of the app
+  2) Handle registering the user on first-time login
+  3) Routing of the various page-components of the web app via React-Router-DOM
+*/
+
 const App = () => {
+  const { login, accessToken } = useSelector((state) => state.token);
+  // , loginTime
+  const dispatch = useDispatch();
+
+  /**
+   * 1) Called immediately after component's mount takes place
+   * 2) Ideal place to handle pre-setup
+   * 3) Check if the user is already logged in by checking cookies
+   *
+   * NOTE: cookies is not ideal for private data, but since we are not
+   * requesting any private data (just authToken), it is safe to do so here
+   */
+
+  const retrieveOtherData = async () => {
+    try {
+      const res = await SpotifyManager.me(accessToken);
+      dispatch(setUserData({ displayName: res.display_name }));
+    } catch (error) {
+      console.log(error);
+      dispatch(
+        removeUserData({
+          loggedIn: false,
+          showError: true,
+          errors:
+            'Problem with authentication or login session has expired. Please try again.',
+        })
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (!Cookies.get('spotify_auth_state')) {
+      TokenManager.getToken();
+    } else if (login === true) {
+      retrieveOtherData(accessToken);
+    } else {
+      console.log('App -> Not logged in');
+    }
+    // if (new Date().getTime() - loginTime >= 3600 * 1000)
+  }, [login]);
+
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
+          <Route exact path="/callback" component={Callback} />
           <Authroute exact path="/new-playlist" component={NewPlaylist} />
           <Route exact path="/sign_up" component={Register} />
           <Route exact path="/sign_in" component={LogIn} />
