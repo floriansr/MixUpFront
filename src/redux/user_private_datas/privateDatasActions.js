9d500887f73e27e661af0641fef12d76837f0ceb
floriansr <sueurfloriansf@gmail.com>
2020-11-17 10:13:13 +0100

    [create] privateDatasActions

---
+++ src/redux/user_private_datas/privateDatasActions.js
@@ -0,0 +1,14 @@
+import { SET_USER_DATA, REMOVE_USER_DATA } from './privateDatasTypes';
+
+export const setUserData = (data) => {
+  return {
+    type: SET_USER_DATA,
+    details: data,
+  };
+};
+
+export const removeUserData = () => {
+  return {
+    type: REMOVE_USER_DATA,
+  };
+};
