81739ff51094bed3b9a44d699c345e3dbb6425ec
floriansr <sueurfloriansf@gmail.com>
2020-11-17 09:19:08 +0100

    [add] authReducer into redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,13 @@ import thunkMiddleware from 'redux-thunk';
 import logReducer from './log/logReducer';
 import userReducer from './user/userReducer';
 import tracklistReducer from './tracklist/tracklistReducer';
+import spotifyReducer from './spotify_authentification/authReducer';
 
 const rootReducer = combineReducers({
   log: logReducer,
   user: userReducer,
   tracks: tracklistReducer,
+  spotify_authentification: spotifyReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
