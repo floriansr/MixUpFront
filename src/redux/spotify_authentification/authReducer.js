356099fb1379e4eb2564ab956a9159398c57bbce
floriansr <sueurfloriansf@gmail.com>
2020-11-17 09:18:47 +0100

    [create] authSpotifyReducer

---
+++ src/redux/spotify_authentification/authReducer.js
@@ -0,0 +1,40 @@
+import { SET_TOKENS, REMOVE_TOKENS } from './authTypes';
+
+const initialState = {
+  accessToken: '',
+  refreshToken: '',
+  expires_in: 0,
+  scopes: '',
+  login: false,
+  loginTime: '',
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_TOKENS:
+      return {
+        ...state,
+        accessToken: action.accessToken,
+        refreshToken: action.refreshToken,
+        scopes: action.scopes,
+        expires_in: 3600,
+        login: true,
+        loginTime: new Date().getTime(),
+      };
+
+    case REMOVE_TOKENS:
+      return {
+        ...state,
+        accessToken: '',
+        refreshToken: '',
+        scopes: '',
+        expires_in: 0,
+        login: false,
+        loginTime: '',
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
