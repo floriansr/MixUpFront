9df62815fc67e332098ece5c699d72c19cddec3e
floriansr <sueurfloriansf@gmail.com>
2020-11-17 10:12:56 +0100

    [add] userdataReducer into redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,14 @@ import logReducer from './log/logReducer';
 import userReducer from './user/userReducer';
 import tracklistReducer from './tracklist/tracklistReducer';
 import spotifyReducer from './spotify_authentification/authReducer';
+import userdataReducer from './user_private_datas/privateDatasReducer';
 
 const rootReducer = combineReducers({
   log: logReducer,
   user: userReducer,
   tracks: tracklistReducer,
   spotify_authentification: spotifyReducer,
+  private_datas_user: userdataReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
