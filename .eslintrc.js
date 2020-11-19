3fa19018ba91b205ef72a1c24c5358a5d76e90e5
floriansr <sueurfloriansf@gmail.com>
2020-11-19 07:42:18 +0100

    [add] eslint import/sort plugins + regex

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
+  plugins: ['simple-import-sort'],
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
     sourceType: 'module', // Allows using import/export statements
@@ -29,15 +30,35 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
     'plugin:import/errors',
-    'plugin:import/warnings'
+    'plugin:import/warnings',
+    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
     'react/prop-types': ['off'],
     'react/jsx-props-no-spreading': ['off'],
     'no-console': ['off'],
     'no-underscore-dangle': ['off'],
+    'jsx-a11y/media-has-caption': ['off'],
+    'no-unused-vars': ['off'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^@?\\w'],
+          ['^\\u0000'],
+          ['^assets(/.*|$)'],
+          ['^react'],
+          ['^redux(/.*|$)'],
+          ['^constants(/.*|$)'],
+          ['^services(/.*|$)'],
+          ['^tools(/.*|$)'],
+          ['^components(/.*|$)'],
+          ['^pages(/.*|$)'],
+          ['^\\.']
+        ]
+      }
+    ],
     quotes: [2, 'single', { avoidEscape: true }],
     'jsx-a11y/label-has-associated-control': [
       'warn',
